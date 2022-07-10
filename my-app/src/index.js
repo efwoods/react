f45d30102043e6c8f8aa55d5687f40b2bfba3d34
efwoods <22337004+efwoods@users.noreply.github.com>
2022-07-10 11:51:58 -0400

    completed displaying col and row

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -57,6 +57,7 @@ class Game extends React.Component {
             
         };
     }
+    
     handleClick(i) {
         const history = this.state.history.slice(0, this.state.stepNumber + 1);
         const current = history[history.length - 1];
@@ -94,7 +95,6 @@ class Game extends React.Component {
         const history = this.state.history;
         const current = history[this.state.stepNumber];
         const winner = calculateWinner(current.squares);
-
 
         const moves = history.map((step, move) => {
             const desc = move ?
